69ffa6794ad87dc47bc100adeb15906fa3a2a5dc
Pham Tuan Linh Nguyen <pham.tuan.linh.nguyen@rwth-aachen.de>
2023-12-26 17:07:09 +0100

    Change Listening Samples

--- equalise_loudness.py
+++ equalise_loudness.py
@@ -1,14 +1,31 @@
 import soundfile as sf
 import pyloudnorm as pyln 
 
-data, sample_rate = sf.read("/Users/nptlinh/Desktop/BA-Code/test_set/sine.wav")
 
-# measure the loudness first 
-meter = pyln.Meter(sample_rate) # create BS.1770 meter
-loudness = meter.integrated_loudness(data)
-print(loudness)
-# #loudness normalize audio to -12 dB LUFS
-# loudness_normalized_audio = pyln.normalize.loudness(data, loudness, -23.0)
+def modify_loudness(files, norm_loudness):
+    for type, filepath in files.items():
+        data, sample_rate = sf.read(filepath)
 
-# # Writing a WAV file
-# sf.write('/Users/nptlinh/Desktop/BA-Code/test_set/sine.wav', loudness_normalized_audio, sample_rate)
+        # measure the loudness first 
+        meter = pyln.Meter(sample_rate) # create BS.1770 meter
+        loudness = meter.integrated_loudness(data)
+        print(loudness)
+        #loudness normalize audio to -40 dB LUFS
+        loudness_normalized_audio = pyln.normalize.loudness(data, loudness, norm_loudness)
+
+        # Writing a WAV file
+        sf.write(f'/Users/nptlinh/Desktop/BA-Code/test_set/{type}_{norm_loudness}.wav', loudness_normalized_audio, sample_rate)
+
+
+
+files = {
+    "airplane": "/Users/nptlinh/Desktop/BA-Code/test_set/Inside_Aircraft2_binaural ( 0.00-30.00 s).wav",
+    "train": "/Users/nptlinh/Desktop/BA-Code/test_set/Inside_Train_Noise1_binaural ( 0.00-30.00 s).wav",
+    "car": "/Users/nptlinh/Desktop/BA-Code/test_set/Midsize_Car1_100Kmh_binaural ( 0.00-30.00 s).wav",
+    "nature_creek": "/Users/nptlinh/Desktop/BA-Code/test_set/Nature2_Creek_binaural ( 0.00-30.00 s).wav",
+    "public": "/Users/nptlinh/Desktop/BA-Code/test_set/Pub_Noise_Binaural_V2.wav",
+    "open_field": "/Users/nptlinh/Desktop/BA-Code/test_set/Nature3_Open_Field_Noise_binaural ( 0.00-30.00 s).wav",
+    "sine": "/Users/nptlinh/Desktop/BA-Code/test_set/sine.wav"
+}
+
+modify_loudness(files, -35)
